9797943e1f7a3239852c36adf3384f7d66220531
snisonoff <spencer.nisonoff@stonybrook.edu>
2021-02-15 19:54:05 -0500

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Spencer Nisonoff on 2/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
