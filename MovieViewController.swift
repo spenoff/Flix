e1a2fbe992abc8dd47688656b34a37077f34285f
snisonoff <spencer.nisonoff@stonybrook.edu>
2021-02-17 18:07:27 -0500

    Synced the Table View to code.

---
+++ MovieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieViewController.swift
+//  Flix
+//
+//  Created by Spencer Nisonoff on 2/17/21.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
