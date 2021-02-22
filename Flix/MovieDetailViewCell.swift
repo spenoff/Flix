2b58db73cac31e89f7565ffa2003bf14981a5099
snisonoff <spencer.nisonoff@stonybrook.edu>
2021-02-22 15:31:16 -0500

    Added Navigation Controller and Movie Detail View Controller

---
+++ Flix/MovieDetailViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieDetailViewCell.swift
+//  Flix
+//
+//  Created by Spencer Nisonoff on 2/22/21.
+//
+
+import UIKit
+
+class MovieDetailViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
